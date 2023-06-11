1bf307219de9380cc407aeeb5935f20a82eda401
Rha02 <romuzus08@gmail.com>
2023-06-11 12:56:46 -0700

    Scaffolded App

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+    h1 {
+        @include breakpoint.up("xs") {
+            color: red;
+        }
+        @include breakpoint.up("sm") {
+            color: orange;
+        }
+        @include breakpoint.up("md") {
+            color: blue;
+        }
+        @include breakpoint.up("lg") {
+            color: green;
+        }
+        @include breakpoint.up("xl") {
+            color: purple;
+        }
+    }
+</style>
