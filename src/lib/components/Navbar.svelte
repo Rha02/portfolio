0183ca450ceabbae7bbcacb44c9513ce3a326334
Rha02 <romuzus08@gmail.com>
2023-06-18 19:07:10 -0700

    Navbar compatible with mobile screens

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import MenuIcon from '$lib/icons/Menu.svelte';
+	import XIcon from '$lib/icons/X.svelte';
 
 	interface NavItem {
 		name: string;
@@ -20,38 +22,77 @@
 			href: '/contact'
 		}
 	];
+
+	let isMenuOpen = false;
+
+	const openMenu = () => {
+		isMenuOpen = true;
+	};
+
+	const closeMenu = () => {
+		isMenuOpen = false;
+	};
 </script>
 
 <nav>
-	<a href="/" class="title"> Romuz Abdulhamidov </a>
-	<ul class="nav-links">
-		{#each navItems as item}
-			<li>
-				<a href={item.href} class:active={item.href == $page.url.pathname}>
-					{item.name}
-				</a>
-			</li>
-		{/each}
-	</ul>
+	<div class="top-bar">
+		<a href="/" class="title"> Romuz Abdulhamidov </a>
+		<ul class="nav-links">
+			{#each navItems as item}
+				<li>
+					<a href={item.href} class:active={item.href == $page.url.pathname}>
+						{item.name}
+					</a>
+				</li>
+			{/each}
+		</ul>
+		<div class="nav-links-mobile">
+			{#if isMenuOpen}
+				<button class="menu-button" on:click={closeMenu}>
+					<XIcon stroke="white" width={32} height={32} />
+				</button>
+			{:else}
+				<button class="menu-button" on:click={openMenu}>
+					<MenuIcon stroke="white" width={32} height={32} />
+				</button>
+			{/if}
+		</div>
+	</div>
+	{#if isMenuOpen}
+		<div class="mobile-menu">
+			<ul>
+				{#each navItems as item}
+					<li>
+						<a href={item.href} class:active={item.href == $page.url.pathname}>
+							{item.name}
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	{/if}
 </nav>
 
 <style lang="scss">
 	nav {
+		z-index: 100;
+		position: fixed;
+		width: 100%;
+	}
+	.top-bar {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
 		min-height: 5rem;
-		position: fixed;
 		background-color: var(--bg-color);
-		z-index: 100;
-		width: 100%;
 		border-bottom: var(--border-color) 1px solid;
-		ul {
-			display: flex;
+		.nav-links {
+			display: none;
 			gap: 2rem;
 			margin-right: 1rem;
 			@include breakpoint.up('md') {
 				margin-right: 2rem;
+				display: flex;
 			}
 			@include breakpoint.up('lg') {
 				margin-right: 3rem;
@@ -64,8 +105,8 @@
 				a {
 					color: var(--text-color);
 					text-decoration: none;
-                    transition: 0.2s;
-                    padding: 12px;
+					transition: 0.2s;
+					padding: 12px;
 					&:hover {
 						color: var(--cyan-color);
 					}
@@ -73,6 +114,29 @@
 						color: var(--cyan-color);
 						text-shadow: 0 0 2px var(--cyan-color);
 					}
+				}
+			}
+		}
+		.nav-links-mobile {
+			margin-right: 1rem;
+			@include breakpoint.up('md') {
+				display: none;
+			}
+			.menu-button {
+				padding: 0.5rem 1rem;
+				cursor: pointer;
+				transition: 0.15s;
+				outline: none;
+				color: white;
+				background-color: transparent;
+				border: none;
+				border-radius: 5px;
+				&:hover,
+				&:focus {
+					background-color: rgba(255, 255, 255, 0.1);
+				}
+				&:active {
+					background-color: rgba(255, 255, 255, 0.2);
 				}
 			}
 		}
@@ -91,6 +155,39 @@
 			}
 			@include breakpoint.up('xl') {
 				margin-left: 4rem;
+			}
+		}
+	}
+	.mobile-menu {
+		display: flex;
+		justify-content: end;
+		@include breakpoint.up('md') {
+			display: none;
+		}
+		ul {
+			margin: -0.5rem 0.5rem;
+			padding: 0;
+			list-style: none;
+			width: 45%;
+			max-width: 14rem;
+			border: 1px solid var(--border-color);
+			border-radius: 5px;
+			background-color: var(--bg-color);
+			box-shadow: 0 0 4px rgba(255, 255, 255, 0.2);
+			li {
+				padding: 0.5rem 1rem;
+				&:hover {
+					background-color: rgba(0, 0, 0, 0.1);
+				}
+				a {
+					color: var(--text-color);
+					text-decoration: none;
+					transition: 0.2s;
+					&.active {
+						color: var(--cyan-color);
+						text-shadow: 0 0 2px var(--cyan-color);
+					}
+				}
 			}
 		}
 	}
---
+++ src/lib/icons/Menu.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let width = 64;
+	export let height = 64;
+	export let fill = 'none';
+	export let stroke = '#000000';
+</script>
+
+<svg
+	{width}
+    {height}
+	viewBox="0 0 20 20"
+	xmlns="http://www.w3.org/2000/svg"
+	fill={fill}
+>
+	<path
+		fill={stroke}
+		fill-rule="evenodd"
+		d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"
+	/>
+</svg>
